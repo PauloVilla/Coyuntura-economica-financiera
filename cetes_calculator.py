a04b4d7acd3ccc7d6932211facb8032e15905ea5
Vidal <vidal@cerby.com>
2023-10-16 21:20:41 -0600

    Actualizar calculadora de Cetes

--- cetes_calculator.py
+++ cetes_calculator.py
@@ -1,40 +1,69 @@
 import math
 
+import numpy as np
 import pandas as pd
 
 
 class resultado:
-    def __init__(self, capital_total, interes, isr):
+    def __init__(self, capital_total, interes, isr, periodos):
         self.capital_total = capital_total
         self.interes = interes
         self.isr = isr
+        self.periodos = periodos
 
 
-def calculo_cetes(capital, plazos):
-    data = []
-    for plazo in plazos:
-        capital = float(capital)
+def calculo_cetes(capital_inicial, plazo, años):
+
+    tasas = np.array(
+        [[0.06, 0.07, 0.08, 0.09, 0.10, 0.09, 0.11, 0.12, 0.13, 0.14, 0.15]])
+    periodos = int(años)*364/int(plazo)
+    periodos = math.floor(periodos)
+    capital_total = capital_inicial
+    interes_neto_total = 0
+    años_transurridos = 0
+    tasa = 0.1105
+
+    for i in range(periodos):
+
+        if i > 0:
+            if plazo == 28:
+                if i % 13 == 0:
+                    años_transurridos = años_transurridos+1
+                    tasas = np.append(tasas, np.mean(tasas))
+                    tasas = np.delete(tasas, 0)
+            if plazo == 91:
+                if i % 4 == 0:
+                    años_transurridos = años_transurridos+1
+                    tasas = np.append(tasas, np.mean(tasas))
+                    tasas = np.delete(tasas, 0)
+            if plazo == 182:
+                if i % 2 == 0:
+                    años_transurridos = años_transurridos+1
+                    tasas = np.append(tasas, np.mean(tasas))
+                    tasas = np.delete(tasas, 0)
+            if plazo == 364:
+                años_transurridos = años_transurridos+1
+                tasas = np.append(tasas, np.mean(tasas))
+                tasas = np.delete(tasas, 0)
+
+        tasa = np.mean(tasas)
+        capital = float(capital_total)
         plazo = float(plazo)
-        tasa = 0.1105
-        precio = 10 / (1 + (tasa / 360) * plazo)
+        precio = 10 / (1 + (tasa / 364) * plazo)
         titulos = math.floor(capital/precio)
         remanente = capital % precio
         inversion_cetes = precio*titulos
-        interes_bruto = inversion_cetes*(tasa / 360)*plazo
+        interes_bruto = inversion_cetes*(tasa / 364)*plazo
         isr = interes_bruto*0.015
         utilidad_neta = interes_bruto - isr
         capital_total = utilidad_neta+remanente+capital
-        capital_total = "${:,.2f}".format(capital_total)
-        utilidad_neta = "${:,.2f}".format(utilidad_neta)
+        interes_neto_total = interes_neto_total + utilidad_neta
 
-        res = resultado(capital_total, utilidad_neta, isr)
+    capital_total = "${:,.2f}".format(capital_total)
+    utilidad_neta = "${:,.2f}".format(utilidad_neta)
+    data = [['Capital Inicial', "${:,.2f}".format(float(capital))], ['Plazo', f"{plazo} días"], ['Periodos', periodos], [
+        'Interes Neto', "${:,.2f}".format(float(interes_neto_total))], ['Capital Final', capital_total]]
 
-        data.append(["${:,.2f}".format(float(capital)), str(
-            plazo) + " días", res.interes, res.capital_total])
+    df = pd.DataFrame(data, columns=['Concepto', 'Valor'])
 
-    # Create the pandas DataFrame
-    df = pd.DataFrame(
-        data, columns=['Capital Inicial', 'Plazo', 'Interes Neto', 'Capital Final'])
     return df
-
-    return pd
--- pages/Calculadora_de_Cetes.py
+++ pages/Calculadora_de_Cetes.py
@@ -5,17 +5,26 @@ from cetes_calculator import calculo_cetes
 number_input = None
 
 
-def cambio_capital(key):
+def cambio_parametros(key):
+    print(f"Change on {key}")
     try:
-        capital = float(st.session_state[key])
-    except:
-        st.write("Formato inválido.")
+        capital = float(st.session_state["capital_cetes"])
+        periodo = float(st.session_state["periodo"])
+        anios_invertir = int(st.session_state["anios_invertir"])
+    except Exception as e:
+        print(e)
         return
     table = st.table(calculo_cetes(
-        capital, [28, 91, 182, 364]))
+        capital, periodo, anios_invertir))
 
 
-number_input = st.text_input(
-    "Capital Inicial", placeholder="Entrar una cantidad", key="capital_cetes",  on_change=cambio_capital, args=("capital_cetes",))
-
-table_placeholder = st.container()
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.text_input(
+        "Capital Inicial", placeholder="Entrar una cantidad de capital incial", key="capital_cetes",  on_change=cambio_parametros, args=("capital_cetes",))
+with col2:
+    st.selectbox(
+        "Periodo de Inversión", options=[28, 91, 182, 364], key="periodo",  on_change=cambio_parametros, args=("periodo",))
+with col3:
+    st.text_input(
+        "Años de Inversión", placeholder="Entrar cantidad de años a inveritr", key="anios_invertir",  on_change=cambio_parametros, args=("anios_invertir",))
