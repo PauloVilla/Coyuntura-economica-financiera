a8c6173c0bbc95f4991785c04103647da815c3a5
Vidal <vidal@cerby.com>
2023-11-03 20:59:47 -0600

    Mejorar presentación de calculadora de Cetes

--- pages/Calculadora_de_Cetes.py
+++ pages/Calculadora_de_Cetes.py
@@ -1,12 +1,25 @@
 import streamlit as st
+from pandas import DataFrame
 
 from cetes_calculator import calculo_cetes
 
 number_input = None
 
+with st.container():
+    st.title("Calculadora de Cetes")
+    st.markdown(
+        "Esta calculadora de Cetes te permite calcular el valor de tu inversión a un cierto plazo de años.")
+    st.markdown(
+        "Módifica el capital inicial, periodo de inversión y años a invertir a tu gusto. La calculadora te dirá el monto en capital final que tendrás después del periodo de inversión.")
+    st.divider()
+col1, col2, col3, col4 = st.columns(4)
+st.divider()
+df = DataFrame()
+container = st.container()
+container.empty()
 
-def cambio_parametros(key):
-    print(f"Change on {key}")
+
+def cambio_parametros():
     try:
         capital = float(st.session_state["capital_cetes"])
         periodo = float(st.session_state["periodo"])
@@ -14,17 +27,31 @@ def cambio_parametros(key):
     except Exception as e:
         print(e)
         return
-    table = st.table(calculo_cetes(
-        capital, periodo, anios_invertir))
+    with container:
+        st.table(calculo_cetes(
+            capital, periodo, anios_invertir))
 
 
-col1, col2, col3 = st.columns(3)
 with col1:
     st.text_input(
-        "Capital Inicial", placeholder="Entrar una cantidad de capital incial", key="capital_cetes",  on_change=cambio_parametros, args=("capital_cetes",))
+        "Capital Inicial", value=10000, placeholder="Entrar una cantidad de capital incial", key="capital_cetes")
 with col2:
     st.selectbox(
-        "Periodo de Inversión", options=[28, 91, 182, 364], key="periodo",  on_change=cambio_parametros, args=("periodo",))
+        "Periodo de Inversión", options=[28, 91, 182, 364], key="periodo")
 with col3:
     st.text_input(
-        "Años de Inversión", placeholder="Entrar cantidad de años a inveritr", key="anios_invertir",  on_change=cambio_parametros, args=("anios_invertir",))
+        "Años de Inversión", value=2, placeholder="Entrar cantidad de años a inveritr", key="anios_invertir")
+
+with col4:
+    st.button("Hacer Cálculo", key="calc_button",
+              on_click=cambio_parametros, use_container_width=True)
+
+st.write(
+    """<style>
+    [data-testid="baseButton-secondary"] {
+        margin-top: 28px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
